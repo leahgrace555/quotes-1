83e7e91a1f0b4ee241307c051253e96c5294642f
Matthew <Mattpet26@gmail.com>
2020-09-23 18:21:58 -0700

    completed mvp!

--- src/test/java/quotes/testJavaQuote.java
+++ src/test/java/quotes/testJavaQuote.java
@@ -1,4 +1,14 @@
 package quotes;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class testJavaQuote {
+    @Test
+    public void testConstruct(){
+        Quote testQuote = new Quote("charlie", "Yo, this wack.");
+
+        assertEquals("charlie was written by Yo, this wack.", testQuote.toString());
+    }
 }
